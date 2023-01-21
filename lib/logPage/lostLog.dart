d3df7ae85633233b8c6d0d5d4c59e96e9e4d1500
taiyomaruyama <stickytomatobaby0501@gmail.com>
2023-01-21 23:11:16 +0900

    location commit.

--- lib/logPage/lostLog.dart
+++ lib/logPage/lostLog.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:prevention_lost_item/tools/customFonts.dart';
 import 'package:prevention_lost_item/tools/getLocation.dart';
+import 'dart:async';
+import 'dart:math';
 
+import 'package:geolocator/geolocator.dart';
 import 'logPageBrain/topCardWidget.dart';
 
 class LostItemLogPage extends StatefulWidget {
@@ -22,16 +25,100 @@ lostItem(List lists) {
 }
 
 class _LostItemLogPageState extends State<LostItemLogPage> {
-  List logTimeLIst = [
-    '09:00',
-    '09:00',
-    '09:00',
-  ];
-  List logLocationList = [
-    '大阪府西淀川区です。',
-    '大阪府西淀川区です。',
-    '大阪府西淀川区です。',
-  ];
+  List logLocationList = [];
+
+  double Latitude = 0;
+  double Longitude = 0;
+  late Timer timered;
+  late int getLocationCounter = 0;
+  double twoPointDistance = 0;
+  double betweenTwoPointSpeed = 0;
+  List<double> LatitudePointList = [];
+  List<double> LongitudePointList = [];
+  List<double> averageSpeedList = [];
+  double speedAverage = 0;
+  int speedRankBefore = 0;
+  late int speedRankAfter;
+  bool judge = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    getEverTimeLocation();
+  }
+
+  // Geolocatorで現在地の取得
+  void getLocation() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('現在地を取得できません。');
+      }
+    }
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    Latitude = position.latitude;
+    Longitude = position.longitude;
+  }
+
+  void getEverTimeLocation() {
+    timered = Timer.periodic(const Duration(seconds: 3), (timer) {
+      getLocation();
+      getLocationCounter++;
+      if (getLocationCounter <= 2) {
+        LatitudePointList.add(Latitude);
+        LongitudePointList.add(Longitude);
+      } else {
+        LatitudePointList.add(Latitude);
+        LongitudePointList.add(Longitude);
+        LatitudePointList.removeAt(0);
+        LongitudePointList.removeAt(0);
+        double latitudeDifference = LatitudePointList[1] - LatitudePointList[0];
+        double longitudeDifference =
+            LongitudePointList[1] - LongitudePointList[0];
+        twoPointDistance =
+            sqrt(pow(latitudeDifference, 2) + pow(longitudeDifference, 2));
+        betweenTwoPointSpeed = twoPointDistance * 10000 / 2;
+        averageSpeed(betweenTwoPointSpeed);
+        setState(() {});
+      }
+    });
+    timered;
+  }
+
+  void averageSpeed(double speed) {
+    averageSpeedList.add(speed);
+    if (averageSpeedList.length == 9) {
+      speedAverage = averageSpeedList.reduce((a, b) => a + b) / 10;
+      averageSpeedList.clear();
+      if (speedAverage <= 1) {
+        speedRankAfter = 1;
+        speedRankBefore == speedRankAfter ? judge = true : judge = false;
+        speedRankBefore = 1;
+      } else if (speedAverage <= 2) {
+        speedRankAfter = 2;
+        speedRankBefore == speedRankAfter ? judge = true : judge = false;
+        speedRankBefore = 2;
+      } else {
+        speedRankAfter = 3;
+        speedRankBefore == speedRankAfter ? judge = true : judge = false;
+        speedRankBefore = 3;
+      }
+      logAction(judge);
+    }
+  }
+
+  void logAction(bool judge) {
+    if (judge == true) {
+      logLocationList.add(Latitude);
+    }
+  }
+
+  void timerStop() {
+    timered!.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,10 +138,7 @@ class _LostItemLogPageState extends State<LostItemLogPage> {
                   icon: const Icon(Icons.arrow_back_ios),
                 ),
                 IconButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => getLocation()));
-                  },
+                  onPressed: () {},
                   icon: const Icon(Icons.menu_outlined),
                 ),
               ],
@@ -68,8 +152,9 @@ class _LostItemLogPageState extends State<LostItemLogPage> {
               height: 10.0,
             ),
             Expanded(
+              flex: 9,
               child: ListView.builder(
-                itemCount: logTimeLIst.length,
+                itemCount: logLocationList.length,
                 itemBuilder: (BuildContext context, int index) {
                   return Container(
                     margin: const EdgeInsets.only(bottom: 5.0),
@@ -81,30 +166,23 @@ class _LostItemLogPageState extends State<LostItemLogPage> {
                     ),
                     child: Row(
                       children: [
-                        Expanded(
-                          flex: 1,
-                          child: Text(
-                            logTimeLIst[index],
-                            style: customFont02,
-                          ),
+                        Text(
+                          logLocationList[index].toString(),
+                          style: customFont02,
                         ),
-                        Expanded(
-                          flex: 4,
-                          child: Text(
-                            logLocationList[index],
-                            style: customFont02,
-                          ),
-                        ),
+                        Text(Latitude.toString()),
                       ],
                     ),
                   );
                 },
               ),
             ),
+            Expanded(flex: 1, child: Text(Latitude.toString())),
+            Expanded(flex: 1, child: Text(Longitude.toString())),
+            Expanded(flex: 1, child: Text(speedAverage.toString())),
           ],
         ),
       ),
     );
   }
 }
-
