aa25cfe2e49f89b7faae21a3a5a4cdc2405a5734
taiyomaruyama <stickytomatobaby0501@gmail.com>
2023-01-25 22:08:58 +0900

    new code type.

--- lib/logPage/lostLog.dart
+++ lib/logPage/lostLog.dart
@@ -24,119 +24,175 @@ lostItem(List lists) {
 }
 
 class _LostItemLogPageState extends State<LostItemLogPage> {
-  List logTimeList = [];
-  List logLocationList = [];
-
-  double Latitude = 0;
-  double Longitude = 0;
-  late Timer timered;
-  late int getLocationCounter = 0;
-  double twoPointDistance = 0;
-  double betweenTwoPointSpeed = 0;
-  List<double> LatitudePointList = [];
-  List<double> LongitudePointList = [];
-  List<double> averageSpeedList = [];
-  double speedAverage = 0;
-  int speedRankBefore = 0;
-  late int speedRankAfter;
-  bool judge = true;
-
-  //for test
-  List LatitudeLatitudeList = [];
-
   @override
   void initState() {
     super.initState();
-    getLocation();
-    getEverTimeLocation();
+    getLocationAndSpeed();
   }
 
-  void getLocation() async {
-    Location location = new Location();
+  //List
+  late List speedList;
+  late List latitudeList;
+  late List longitudeList;
 
-    bool _serviceEnabled;
-    PermissionStatus _permissionGranted;
-    LocationData _locationData;
+  late Timer timered;
 
-    _serviceEnabled = await location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await location.requestService();
-      if (!_serviceEnabled) {
+  //get location and then speed function.
+  void getLocationAndSpeed() async {
+    Location location = Location();
+
+    bool serviceEnabled = false;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
         return;
       }
     }
-    _permissionGranted = await location.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await location.requestPermission();
-      if (_permissionGranted != PermissionStatus.granted) {
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
         return;
       }
     }
-    _locationData = await location.getLocation();
-    Latitude = _locationData.speed!;
-    Longitude = _locationData.longitude!;
-    LatitudeLatitudeList.add(Latitude);
+    locationData = await location.getLocation();
+    speedList.add(locationData.speed);
+    latitudeList.add(locationData.latitude);
+    longitudeList.add(locationData.longitude);
   }
 
-  void getEverTimeLocation() {
-    timered = Timer.periodic(const Duration(seconds: 8), (timer) {
-      getLocation();
-      getLocationCounter++;
-      if (getLocationCounter <= 2) {
-        LatitudePointList.add(Latitude);
-        LongitudePointList.add(Longitude);
-      } else {
-        LatitudePointList.add(Latitude);
-        LongitudePointList.add(Longitude);
-        LatitudePointList.removeAt(0);
-        LongitudePointList.removeAt(0);
-        double latitudeDifference = LatitudePointList[1] - LatitudePointList[0];
-        double longitudeDifference =
-            LongitudePointList[1] - LongitudePointList[0];
-        twoPointDistance =
-            sqrt(pow(latitudeDifference, 2) + pow(longitudeDifference, 2));
-        betweenTwoPointSpeed = twoPointDistance * 10000 / 2;
-        averageSpeed(betweenTwoPointSpeed);
-        setState(() {});
-      }
+  //get every time location function.
+  void getLocationEveryTime () {
+    timered = Timer.periodic(const Duration(seconds: 10), (timer) {
+      getLocationAndSpeed();
     });
-    timered;
   }
 
-  void averageSpeed(double speed) {
-    averageSpeedList.add(speed);
-    if (averageSpeedList.length == 9) {
-      speedAverage = averageSpeedList.reduce((a, b) => a + b) / 10;
-      averageSpeedList.clear();
-      if (speedAverage <= 1) {
-        speedRankAfter = 1;
-        speedRankBefore == speedRankAfter ? judge = true : judge = false;
-        speedRankBefore = 1;
-      } else if (speedAverage <= 2) {
-        speedRankAfter = 2;
-        speedRankBefore == speedRankAfter ? judge = true : judge = false;
-        speedRankBefore = 2;
-      } else {
-        speedRankAfter = 3;
-        speedRankBefore == speedRankAfter ? judge = true : judge = false;
-        speedRankBefore = 3;
-      }
-      logAction(judge);
-    }
+  //judge speed rank.
+  void judgeSpeed () {
+
   }
 
-  void logAction(bool judge) async {
-    if (judge == false) {
-      DateTime now = DateTime.now();
-      logTimeList.add(now.toString().substring(11, 16));
-      logLocationList.add(Latitude);
-      judge = true;
-    }
-  }
 
-  void timerStop() {
-    timered!.cancel();
-  }
+
+
+
+  // List logTimeList = [];
+  // List logLocationList = [];
+  //
+  // double Latitude = 0;
+  // double Longitude = 0;
+  // late Timer timered;
+  // late int getLocationCounter = 0;
+  // double twoPointDistance = 0;
+  // double betweenTwoPointSpeed = 0;
+  // List<double> LatitudePointList = [];
+  // List<double> LongitudePointList = [];
+  // List<double> averageSpeedList = [];
+  // double speedAverage = 0;
+  // int speedRankBefore = 0;
+  // late int speedRankAfter;
+  // bool judge = true;
+  //
+  // //for test
+  // List LatitudeLatitudeList = [];
+  //
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   getLocation();
+  //   getEverTimeLocation();
+  // }
+  // void getLocation() async {
+  //   Location location = new Location();
+  //
+  //   bool _serviceEnabled;
+  //   PermissionStatus _permissionGranted;
+  //   LocationData _locationData;
+  //
+  //   _serviceEnabled = await location.serviceEnabled();
+  //   if (!_serviceEnabled) {
+  //     _serviceEnabled = await location.requestService();
+  //     if (!_serviceEnabled) {
+  //       return;
+  //     }
+  //   }
+  //   _permissionGranted = await location.hasPermission();
+  //   if (_permissionGranted == PermissionStatus.denied) {
+  //     _permissionGranted = await location.requestPermission();
+  //     if (_permissionGranted != PermissionStatus.granted) {
+  //       return;
+  //     }
+  //   }
+  //   _locationData = await location.getLocation();
+  //   Latitude = _locationData.speed!;
+  //   Longitude = _locationData.longitude!;
+  //   LatitudeLatitudeList.add(Latitude);
+  // }
+  //
+  // void getEverTimeLocation() {
+  //   timered = Timer.periodic(const Duration(seconds: 8), (timer) {
+  //     getLocation();
+  //     getLocationCounter++;
+  //     if (getLocationCounter <= 2) {
+  //       LatitudePointList.add(Latitude);
+  //       LongitudePointList.add(Longitude);
+  //     } else {
+  //       LatitudePointList.add(Latitude);
+  //       LongitudePointList.add(Longitude);
+  //       LatitudePointList.removeAt(0);
+  //       LongitudePointList.removeAt(0);
+  //       double latitudeDifference = LatitudePointList[1] - LatitudePointList[0];
+  //       double longitudeDifference =
+  //           LongitudePointList[1] - LongitudePointList[0];
+  //       twoPointDistance =
+  //           sqrt(pow(latitudeDifference, 2) + pow(longitudeDifference, 2));
+  //       betweenTwoPointSpeed = twoPointDistance * 10000 / 2;
+  //       averageSpeed(betweenTwoPointSpeed);
+  //       setState(() {});
+  //     }
+  //   });
+  //   timered;
+  // }
+  //
+  // void averageSpeed(double speed) {
+  //   averageSpeedList.add(speed);
+  //   if (averageSpeedList.length == 9) {
+  //     speedAverage = averageSpeedList.reduce((a, b) => a + b) / 10;
+  //     averageSpeedList.clear();
+  //     if (speedAverage <= 1) {
+  //       speedRankAfter = 1;
+  //       speedRankBefore == speedRankAfter ? judge = true : judge = false;
+  //       speedRankBefore = 1;
+  //     } else if (speedAverage <= 2) {
+  //       speedRankAfter = 2;
+  //       speedRankBefore == speedRankAfter ? judge = true : judge = false;
+  //       speedRankBefore = 2;
+  //     } else {
+  //       speedRankAfter = 3;
+  //       speedRankBefore == speedRankAfter ? judge = true : judge = false;
+  //       speedRankBefore = 3;
+  //     }
+  //     logAction(judge);
+  //   }
+  // }
+  //
+  // void logAction(bool judge) async {
+  //   if (judge == false) {
+  //     DateTime now = DateTime.now();
+  //     logTimeList.add(now.toString().substring(11, 16));
+  //     logLocationList.add(Latitude);
+  //     judge = true;
+  //   }
+  // }
+  //
+  // void timerStop() {
+  //   timered!.cancel();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -161,73 +217,73 @@ class _LostItemLogPageState extends State<LostItemLogPage> {
                 ),
               ],
             ),
-            SizedBox(
-              height: 120,
-              width: double.infinity,
-              child: LogPageTopCard(widget: widget),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            Expanded(
-              flex: 10,
-              child: ListView.builder(
-                itemCount: logLocationList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    margin: const EdgeInsets.only(bottom: 5.0),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 15.0, vertical: 10.0),
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.3),
-                          blurRadius: 6.0,
-                          offset: const Offset(0, 5),
-                        ),
-                      ],
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    child: Row(
-                      children: [
-                        Expanded(
-                            flex: 2,
-                            child:
-                                Text(logTimeList[index], style: customFont02)),
-                        Expanded(
-                            flex: 4,
-                            child: Text(logLocationList[index].toString(),
-                                style: customFont02)),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
-
-            //for test
-            Expanded(
-              flex: 8,
-              child: ListView.builder(
-                itemCount: LatitudeLatitudeList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    child: Text(
-                        LatitudeLatitudeList[index].toString(),
-                        style: customFont02,
-                    ),
-                  );
-                },
-              ),
-            ),
-            //test end
-
-            Expanded(
-                flex: 1, child: Text('speed : ${Latitude.toString()} m/s')),
-            Expanded(flex: 1, child: Text(Longitude.toString())),
-            Expanded(flex: 1, child: Text(averageSpeedList.toString())),
-            Expanded(flex: 1, child: Text(getLocationCounter.toString())),
+            //   SizedBox(
+            //     height: 120,
+            //     width: double.infinity,
+            //     child: LogPageTopCard(widget: widget),
+            //   ),
+            //   const SizedBox(
+            //     height: 10.0,
+            //   ),
+            //   Expanded(
+            //     flex: 10,
+            //     child: ListView.builder(
+            //       itemCount: logLocationList.length,
+            //       itemBuilder: (BuildContext context, int index) {
+            //         return Container(
+            //           margin: const EdgeInsets.only(bottom: 5.0),
+            //           padding: const EdgeInsets.symmetric(
+            //               horizontal: 15.0, vertical: 10.0),
+            //           decoration: BoxDecoration(
+            //             boxShadow: [
+            //               BoxShadow(
+            //                 color: Colors.black.withOpacity(0.3),
+            //                 blurRadius: 6.0,
+            //                 offset: const Offset(0, 5),
+            //               ),
+            //             ],
+            //             color: Colors.white,
+            //             borderRadius: BorderRadius.circular(15.0),
+            //           ),
+            //           child: Row(
+            //             children: [
+            //               Expanded(
+            //                   flex: 2,
+            //                   child:
+            //                       Text(logTimeList[index], style: customFont02)),
+            //               Expanded(
+            //                   flex: 4,
+            //                   child: Text(logLocationList[index].toString(),
+            //                       style: customFont02)),
+            //             ],
+            //           ),
+            //         );
+            //       },
+            //     ),
+            //   ),
+            //
+            //   //for test
+            //   Expanded(
+            //     flex: 8,
+            //     child: ListView.builder(
+            //       itemCount: LatitudeLatitudeList.length,
+            //       itemBuilder: (BuildContext context, int index) {
+            //         return Container(
+            //           child: Text(
+            //               LatitudeLatitudeList[index].toString(),
+            //               style: customFont02,
+            //           ),
+            //         );
+            //       },
+            //     ),
+            //   ),
+            //   //test end
+            //
+            //   Expanded(
+            //       flex: 1, child: Text('speed : ${Latitude.toString()} m/s')),
+            //   Expanded(flex: 1, child: Text(Longitude.toString())),
+            //   Expanded(flex: 1, child: Text(averageSpeedList.toString())),
+            //   Expanded(flex: 1, child: Text(getLocationCounter.toString())),
           ],
         ),
       ),
