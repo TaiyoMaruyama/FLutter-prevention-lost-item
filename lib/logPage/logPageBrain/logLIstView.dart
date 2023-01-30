a14bccd8cee24fa02a7ff822a8f59648abe82233
taiyomaruyama <stickytomatobaby0501@gmail.com>
2023-01-30 10:52:03 +0900

    add Dismissible.

--- lib/logPage/logPageBrain/logLIstView.dart
+++ lib/logPage/logPageBrain/logLIstView.dart
@@ -21,28 +21,41 @@ class logListView extends StatelessWidget {
     return ListView.builder(
       itemCount: speedLogList.length,
       itemBuilder: (BuildContext context, int index) {
-        return Container(
-          margin: const EdgeInsets.only(bottom: 5.0),
-          padding: const EdgeInsets.symmetric(
-              horizontal: 15.0, vertical: 10.0),
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.3),
-                blurRadius: 6.0,
-                offset: const Offset(0, 5),
-              ),
-            ],
-            color: Color(0xFF006400),
-            borderRadius: BorderRadius.circular(15.0),
+        return Dismissible(
+          key: Key(speedLogList.toString()),
+          onDismissed: (direction) {
+            speedLogList.removeAt(index);
+          },
+          background: Container(
+            color: const Color(0xFF006400),
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text(timeLogList[index], style: customFont03),
-              Text('緯度：${latitudeLogList[index]}', style: customFont03),
-              Text('経度：${longitudeLogList[index]}', style: customFont03),
-            ],
+          child: Container(
+            margin: const EdgeInsets.only(bottom: 5.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  blurRadius: 6.0,
+                  offset: const Offset(0, 5),
+                ),
+              ],
+              color: const Color(0xFF006400),
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(timeLogList[index], style: customFont03),
+                Text('緯度：${latitudeLogList[index]}', style: customFont03),
+                Text('経度：${longitudeLogList[index]}', style: customFont03),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.map, color: Colors.white),
+                ),
+              ],
+            ),
           ),
         );
       },
@@ -50,6 +63,4 @@ class logListView extends StatelessWidget {
   }
 }
 
-void backgroundColorChange (){
-
-}
+void backgroundColorChange() {}
